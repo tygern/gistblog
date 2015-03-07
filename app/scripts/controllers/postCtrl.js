7f4acd627f2c6ab4464884762e5f07a8a25d87a2
tygern <tygern@gmail.com>
2015-03-07 10:36:57 -0700

    allow syntax highlighting

--- app/scripts/controllers/postCtrl.js
+++ app/scripts/controllers/postCtrl.js
@@ -1,9 +1,19 @@
 'use strict';
 
 angular.module('gistblogApp')
-  .controller('PostCtrl', function ($scope, $sce, $stateParams, PostService) {
+  .controller('PostCtrl', function ($scope, $document, $sce, $stateParams, PostService) {
     PostService.get($stateParams.title).then(function (result) {
       $scope.postName = result.data.name;
-      $scope.postContent = $sce.trustAsHtml(atob(result.data.content));
+
+      var highlightScript = '<script>' +
+        '_.each(angular.element(document).find("pre"), function (block) {' +
+          'hljs.highlightBlock(block);' +
+        '});' +
+        '</script>' ;
+
+      var postHtml = atob(result.data.content) + highlightScript;
+      $scope.postContent = $sce.trustAsHtml(postHtml);
     });
+
+
   });
